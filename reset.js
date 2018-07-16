7c12b64d9144812ca23272d3a1447e6b9fbcfb75
Boris Mutafov <mutafow@gmail.com>
2018-07-17 01:23:18 +0300

    Reset now resets output.json as well & error alert link to github added

--- reset.js
+++ reset.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 try {
     fs.writeFileSync("./web/result.js", "var data = [];");
+    fs.writeFileSync("./output.json", "");
     console.log('Resetted successfully.');
 } catch(e) {
     console.log('Results file could not be resetted.');
